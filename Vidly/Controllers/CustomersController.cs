91aa034dab87607f69053b443d7006abdad723c8
Tigercat1992 <silenceslider@gmail.com>
2021-10-21 12:02:27 +0630

    first commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>()
+            {
+                new Customer { Id = 1, Name = "John" },
+                new Customer { Id = 2, Name = "Stephen" }
+            };
+
+            var viewModel = new CustomerViewModel()
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customers = new List<Customer>()
+            {
+                new Customer { Id = 1, Name = "John" },
+                new Customer { Id = 2, Name = "Stephen" }
+            };
+
+            var customer = customers.Find(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            Console.WriteLine(customer);
+
+            return View(customer);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>()
+            {
+                new Movie { Name = "Shrek!" },
+                new Movie { Name = "Harry Potter" }
+            };
+
+            var viewModel = new MovieViewModel()
+            {
+                Movies = movies
+            };
+
+            return View(viewModel);
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            //ViewData["RandomMovie"] = movie;
+
+            //ViewBag.RandomMovie = movie;
+
+            //var viewResult = new ViewResult();
+            //viewResult.ViewData.Model = movie;
+            //return viewResult;
+
+            var customers = new List<Customer>()
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int movieId)
+        {
+            return Content("id="+movieId);
+        }
+
+        [Route("movies/release/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult Release(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+    }
+}
